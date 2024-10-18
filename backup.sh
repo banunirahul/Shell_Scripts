f287cdfb2e24db8b85dd2988608ac92f7b45fa36
banunirahul <banunirahul03@gmail.com>
2024-10-18 21:55:34 +0530

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+<<note
+This Script is used to take the backup od any destination path given in the agrgument
+note
+
+
+echo "Date : $(date)"
+
+echo "Formatted date : $(date '+%Y-%m-%d') and $(date '+%Y-%m-%d_%H_%M_%S')"
+
+#Storing in variable
+
+timestamp=$(date '+%Y-%m-%d_%H_%M_%S')
+
+backup_dir="${timestamp}_backup.zip"
+
+echo "*****Backup of $1 Started***********"
+
+zip -r $backup_dir $1
+
+echo "*****Backed up $1 into $backup_dir as zip file***********"
