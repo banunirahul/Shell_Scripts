23fba096df686cc582421da8a2a6b75fbe9bfa64
banunirahul <banunirahul03@gmail.com>
2024-10-18 21:24:35 +0530

    Create ramramSong.sh

---
+++ ramramSong.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#huslte song
+
+<<MC
+This is my song from huslte
+MC
+
+echo "Mc : didi baat karade tere devar se"
+
+echo "MC : meri nikle jaan va ke tevar se"
+
+echo "MC : sir pe baad mundasa karke waar nigahka"
+
+echo "Mc : Lage teer tezz kadi lezar te"
+                                      
