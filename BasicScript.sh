26bec5c65503b6167d24245b7ff1d7ac3be6673e
banunirahul <banunirahul03@gmail.com>
2024-10-18 21:26:58 +0530

    Create BasicScript.sh

---
+++ BasicScript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#This is sholey for TWS
+<<comment
+This is a script file
+used for our scripting file demo class
+comment
+
+echo "Gabbar: kitne admi the"
+
+echo "Kalia: 2 sarkar"
+
+
+echo "Gabbar: Kitna baja hai"
+
+
+date
+
+echo "Gabbar: kitne bajje se class chal rhi"
+
+uptime
